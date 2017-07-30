095084d0b79c30752847a63d75a52c82d6656522
Patryk-Wawa <nadawcamaila@o2.pl>
2017-07-30 18:55:29 +0200

    Value

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//scripts.js
+
+var a;
+var b;
+var value = (a * a) + (2 * a * b) - (b * b);
+
+if (value > 0) {
+	console.log('Wynik dodatni');
+}	else if (value < 0) {
+	console.log('Wynik ujemny');
+}	else (value = 0); { 
+	console.log('Wynik równy ZERO');
+}
